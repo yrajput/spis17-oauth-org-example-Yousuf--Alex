91b8f5258850fcc907ba3c3263d6b3ee7de05575
Alex Kim <ark037@ucsd.edu>
2017-09-05 14:59:25 -0700

    idk

--- webapp.py
+++ webapp.py
@@ -161,25 +161,36 @@ def renderPage1():
 def renderPage2():
     return render_template('page2.html')
 
-def allowed_file(filename):
-  return '.' in filename and \
-    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+#def allowed_file(filename):
+#  return '.' in filename and \
+#    filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/uploader', methods=['GET', 'POST'])
+#@app.route('/uploader', methods=['GET', 'POST'])
+#def upload_file():
+#  if request.method == 'POST': 
+#    if 'file' not in request.files:
+#      flash('No file part') 
+#      return redirect(request.url)
+#    file = request.files['file']
+#    if file.filename == '':
+#      flash('No selected file')
+#      return redirect(request.url)
+#    if file and allowed_file(file.filename):
+#      filename = secure_filename(file.filename)
+#      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#      return redirect(url_for('uploaded_file', filename=filename))
+#  return
+
+@app.route('/upload')
 def upload_file():
-  if request.method == 'POST': 
-    if 'file' not in request.files:
-      flash('No file part') 
-      return redirect(request.url)
-    file = request.files['file']
-    if file.filename == '':
-      flash('No selected file')
-      return redirect(request.url)
-    if file and allowed_file(file.filename):
-      filename = secure_filename(file.filename)
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-      return redirect(url_for('uploaded_file', filename=filename))
-  return
+  return render_template('upload.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+  if request.method == 'POST':
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    return 'file uploaded successfully'
 
 UPLOAD_FOLDER = '/path/to/the/uploads'
 ALLOWED_EXTENTIONS = set(['png', 'jpg', 'jpeg'])
